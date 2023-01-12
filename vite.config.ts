d61aefb46323f53878574a901044628b3b74aa22
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-01-12 14:34:09 +0800

    refactor(esbuild): 🔧 set legal comments

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import minifyHTML from 'rollup-plugin-minify-html-literals'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: { legalComments: 'external' },
   plugins: [
     (minifyHTML as any).default()
   ]
