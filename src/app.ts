d022ef0b9edb60de32f94ad211636a2340e24385
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-01-12 15:31:20 +0800

    chore: ➕ add `material-icons`

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import { LitElement, css, html } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 import litLogo from './assets/lit.svg'
 
+import 'material-icons/iconfont/material-icons.css'
+
 /**
  * An example element.
  *
