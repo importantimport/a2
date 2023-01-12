6459d329b901ebeeed0e6337b8307519361aa9e3
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-01-12 14:28:17 +0800

    chore: ➕ add `rollup-plugin-minify-html-literals`

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,9 @@
 import { defineConfig } from 'vite'
+import minifyHTML from 'rollup-plugin-minify-html-literals'
 
 // https://vitejs.dev/config/
-export default defineConfig({})
+export default defineConfig({
+  plugins: [
+    (minifyHTML as any).default()
+  ]
+})
