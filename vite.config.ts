4a19ae7843d0b8cef6f0874af71ad87b1b8ad131
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-01-17 15:18:06 +0800

    feat: ✨ pwa
    ➕ add vite-plugin-pwa
    🏷️ add vite-plugin-pwa reference
    🔧 split config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, mergeConfig } from 'vite'
 import minifyHTML from 'rollup-plugin-minify-html-literals'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export const baseConfig = defineConfig({
   build: { target: 'es2022' },
   esbuild: { legalComments: 'external' },
   plugins: [(minifyHTML as any).default()],
 })
+
+export default mergeConfig(
+  baseConfig,
+  defineConfig({
+    plugins: [VitePWA({ injectRegister: 'inline' })],
+  })
+)
