48704231ae2bdda0b5aff360d45a3a151f9a4aba
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-01-17 14:18:33 +0800

    chore(vite): 🔧 set build target

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import minifyHTML from 'rollup-plugin-minify-html-literals'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: { target: 'es2022' },
   esbuild: { legalComments: 'external' },
-  plugins: [
-    (minifyHTML as any).default()
-  ]
+  plugins: [(minifyHTML as any).default()],
 })
