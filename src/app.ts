bee46e300fa2bddfefc43489e8c2b14f000095d5
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-01-17 16:39:30 +0800

    perf(material-icons): ⚡️ woff2 only

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import { LitElement, html } from 'lit'
 import { customElement } from 'lit/decorators.js'
 import { Router } from '@lit-labs/router'
 
-import 'material-icons/iconfont/filled.css'
 
 import './components/nav'
 
