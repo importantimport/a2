572a63a16bd0445184847fafc83b796ce92249c7
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-01-17 14:28:10 +0800

    refactor: ♻️ update router

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import { Router } from '@lit-labs/router'
 
 import 'material-icons/iconfont/filled.css'
 
-import './components/index'
 import './components/nav'
 
 // @ts-ignore: Property 'UrlPattern' does not exist 
@@ -12,21 +11,38 @@ if (!globalThis.URLPattern) await import('urlpattern-polyfill')
 
 @customElement('a2z-app')
 export class App extends LitElement {
-  private router = new Router(this, [
-    { path: '/', name: 'Index', render: () => html`<a2z-index></a2z-index>` },
-    {
-      path: '/settings',
-      name: 'Settings',
-      render: () => html`<h1>Settings</h1>`,
-    },
-  ])
-
   render() {
     return html`
       <main>${this.router.outlet()}</main>
       <a2z-nav></a2z-nav>
     `
   }
+
+  private router = new Router(
+    this,
+    [
+      {
+        path: '/',
+        name: 'Index',
+        render: () => html`<a2z-index></a2z-index>`,
+        enter: async () => {
+          await import('./components/index')
+          return true
+        },
+      },
+      {
+        path: '/settings',
+        name: 'Settings',
+        render: () => html`<h1>Settings</h1>`,
+      },
+    ],
+    {
+      fallback: {
+        name: '404',
+        render: () => html`<h1>404 Not Found</h1>`,
+      },
+    }
+  )
 }
 
 declare global {
