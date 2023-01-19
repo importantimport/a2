27dfc41f90116a684893f1f2b923aab24f541ceb
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-01-19 22:44:52 +0800

    refactor(rpc): ♻️ export options

--- src/lib/rpc/channel.ts
+++ src/lib/rpc/channel.ts
@@ -1,6 +1,19 @@
 import type { EventBasedChannel } from 'async-call-rpc'
 
+export type A2ZChannelOptions = {
+  url: string
+  secret?: string
+}
+
 export class A2ZChannel extends EventTarget implements EventBasedChannel {
+  constructor(
+    private options: A2ZChannelOptions = {
+      url: 'http://localhost:6800/jsonrpc',
+    }
+  ) {
+    super()
+  }
+
   on(listener: (data: unknown) => void) {
     const callback = (e: Event): void => listener((e as MessageEvent).data)
     this.addEventListener('message', callback)
@@ -15,26 +28,21 @@ export class A2ZChannel extends EventTarget implements EventBasedChannel {
   }): Promise<void> {
     this.dispatchEvent(
       new MessageEvent('message', {
-        data: await fetch(
-          import.meta.env.A2Z_RPC_URL ?? 'http://localhost:6800/jsonrpc',
-          {
-            method: 'POST',
-            body: JSON.stringify({
-              ...data,
-              method: `aria2.${data.method}`,
-              params: [
-                ...(import.meta.env.A2Z_RPC_SECRET
-                  ? [`token:${import.meta.env.A2Z_RPC_SECRET}`]
-                  : []),
-                ...data.params,
-              ],
-            }),
-            headers: {
-              Accept: 'application/json',
-              'Content-Type': 'application/json',
-            },
-          }
-        ).then((res) => res.json()),
+        data: await fetch(this.options.url, {
+          method: 'POST',
+          body: JSON.stringify({
+            ...data,
+            method: `aria2.${data.method}`,
+            params: [
+              ...(this.options.secret ? [`token:${this.options.secret}`] : []),
+              ...data.params,
+            ],
+          }),
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+        }).then((res) => res.json()),
       })
     )
   }
--- src/lib/rpc/server.ts
+++ src/lib/rpc/server.ts
@@ -4,4 +4,7 @@ import { A2ZChannel } from './channel'
 
 import type { Aria2 } from './types'
 
-export const server = AsyncCall<Aria2>({}, { channel: new A2ZChannel() })
+export const server = AsyncCall<Aria2>({}, { channel: new A2ZChannel({
+  url: import.meta.env.A2Z_RPC_URL,
+  secret: import.meta.env.A2Z_RPC_SECRET
+}) })
