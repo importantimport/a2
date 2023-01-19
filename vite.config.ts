7d569c5598d24e28a53166e2f2b8feea56703f4c
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-01-19 13:54:16 +0800

    refactor: 🧱 update minify plugin
    ➕ add rollup-plugin-html-literals
    ➖ remove rollup-plugin-minify-html-literals

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig, mergeConfig } from 'vite'
-import minifyHTML from 'rollup-plugin-minify-html-literals'
+import template from 'rollup-plugin-html-literals'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export const baseConfig = defineConfig({
   envPrefix: ['A2Z_', 'VITE_'],
   build: { target: 'es2022' },
   esbuild: { legalComments: 'external' },
-  plugins: [(minifyHTML as any).default()],
+  plugins: [template()],
 })
 
 export default mergeConfig(
