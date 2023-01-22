11d2f6b568d9a5ce4d6285fb044c348cd16cfa45
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-01-22 22:18:21 +0800

    refactor: 🏷️ add import meta env types

--- z/src/vite-env.d.ts
+++ z/src/vite-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pwa/client" />
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
+interface ImportMetaEnv extends Readonly<Record<string, string>> {
+  readonly A2Z_RPC_URL?: string
+  readonly A2Z_RPC_SECRET?: string
+}
