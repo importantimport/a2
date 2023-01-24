6a5be77231911f52c38ca43304a8c87fd366243b
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-01-24 14:17:13 +0800

    chore: ➕ add `vite-tsconfig-paths`

--- z/vite.config.ts
+++ z/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig, mergeConfig } from 'vite'
 import template from 'rollup-plugin-html-literals'
 import { VitePWA } from 'vite-plugin-pwa'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export const baseConfig = defineConfig({
   envPrefix: ['A2Z_', 'VITE_'],
   build: { target: 'es2022' },
   esbuild: { legalComments: 'external' },
-  plugins: [template()],
+  plugins: [template(), tsconfigPaths()],
 })
 
 export default mergeConfig(
