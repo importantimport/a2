52d03a3f4c8a513b0ffa29546be347413bbe74fb
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-02-28 16:31:35 +0800

    chore: 🧱 civet
    ➕ add `vite-plugin-civet`
    🔧 recommend danielx.civet

--- z/vite.config.ts
+++ z/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, mergeConfig } from 'vite'
+import civetPlugin from 'vite-plugin-civet'
 import template from 'rollup-plugin-html-literals'
 import { VitePWA } from 'vite-plugin-pwa'
 import tsconfigPaths from 'vite-tsconfig-paths'
@@ -7,7 +8,7 @@ export const baseConfig = defineConfig({
   envPrefix: ['A2Z_', 'VITE_'],
   build: { target: 'es2022' },
   esbuild: { legalComments: 'external' },
-  plugins: [template(), tsconfigPaths()],
+  plugins: [civetPlugin({ stripTypes: true }), template(), tsconfigPaths()],
 })
 
 export default mergeConfig(
