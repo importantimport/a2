7ff620e0e486d462f14ee94ddb1196907e1c4f6c
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-04-16 20:33:35 +0800

    refactor(settings): ♻️ use list-item-link

--- z/src/pages/settings/index.ts
+++ z/src/pages/settings/index.ts
@@ -1,8 +1,9 @@
-import { LitElement, css, html } from 'lit'
+import { LitElement, html } from 'lit'
 import { customElement } from 'lit/decorators.js'
 
 import '@material/web/list/list'
 import '@material/web/list/list-item'
+import '@material/web/list/list-item-link'
 
 @customElement('a2z-settings-index')
 export class SettingsIndex extends LitElement {
@@ -10,25 +11,19 @@ export class SettingsIndex extends LitElement {
     return html`
       <md-list>
         <md-list-item headline="RPC Settings" disabled></md-list-item>
-        <a href="/settings/a2z">
-          <md-list-item headline="A2Z Settings"></md-list-item>
-        </a>
+        <md-list-item-link
+          headline="A2Z Settings"
+          href="/settings/a2z"
+        ></md-list-item-link>
         <md-list-item headline="Aria2 Settings" disabled></md-list-item>
-        <a href="/settings/status">
-          <md-list-item
-            headline="Status"
-            supportingText="click here!"
-          ></md-list-item
-        ></a>
+        <md-list-item-link
+          headline="Status"
+          supportingText="click here!"
+          href="/settings/status"
+        ></md-list-item-link>
       </md-list>
     `
   }
-
-  static styles = css`
-    a {
-      text-decoration: none;
-    }
-  `
 }
 
 declare global {
