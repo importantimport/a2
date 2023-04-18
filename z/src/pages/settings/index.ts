2a7c69b3f6f99a74b358775c747573784c8f619f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-04-18 17:10:06 +0800

    refactor(settings): ♻️ localized

--- z/src/pages/settings/index.ts
+++ z/src/pages/settings/index.ts
@@ -1,23 +1,25 @@
 import { LitElement, html } from 'lit'
 import { customElement } from 'lit/decorators.js'
+import { localized, msg } from '@lit/localize'
 
 import '@material/web/list/list'
 import '@material/web/list/list-item'
 import '@material/web/list/list-item-link'
 
+@localized()
 @customElement('a2z-settings-index')
 export class SettingsIndex extends LitElement {
   render() {
     return html`
       <md-list>
-        <md-list-item headline="RPC Settings" disabled></md-list-item>
+        <md-list-item headline=${msg('RPC Settings')} disabled></md-list-item>
         <md-list-item-link
-          headline="A2Z Settings"
+          headline=${msg('A2Z Settings')}
           href="/settings/a2z"
         ></md-list-item-link>
-        <md-list-item headline="Aria2 Settings" disabled></md-list-item>
+        <md-list-item headline=${msg('Aria2 Settings')} disabled></md-list-item>
         <md-list-item-link
-          headline="Status"
+          headline=${msg('Status')}
           supportingText="click here!"
           href="/settings/status"
         ></md-list-item-link>
