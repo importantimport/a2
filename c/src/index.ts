71352757758ce9461c9e15d3cf5afd9a3c6b0b3b
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-04-19 15:03:29 +0800

    feat(a2c): ✨ export all types

--- c/src/index.ts
+++ c/src/index.ts
@@ -7,4 +7,5 @@ export const createClient = (options: A2ChannelOptions) =>
 
 export { A2Channel, type A2ChannelOptions } from './channel'
 export type { Aria2 } from './methods'
+export type { Status, Uri, File, Peer, Server, TellStatusResult } from './types'
 export type { _AsyncVersionOf } from 'async-call-rpc'
